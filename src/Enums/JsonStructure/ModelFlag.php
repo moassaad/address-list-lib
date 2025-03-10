7b911a2ca03f26579ad8fc6569634ef1093e3c5b
moassaad <mohammadasaadgo@gmail.com>
2025-03-10 15:32:18 +0200

    create: model flag enum.

---
+++ src/Enums/JsonStructure/ModelFlag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Moassaad\Addressia\Enums\JsonStructure;
+
+enum ModelFlag : string
+{
+    case COUNTRY = "country";
+    case GOVERNORATE = "governorate";
+    case CITY = "city";
+    case ADDRESS = "address";
+}
